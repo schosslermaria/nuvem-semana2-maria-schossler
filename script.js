823dbd369eace4a74a89de03f79d1ca987a91e44
mary <marialauraschosslerpereira@gmail.com>
2026-03-02 09:04:22 -0300

    mudanças em a api

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputCidade = document.getElementById("cidade");
+const btnBuscar = document.getElementById("buscar");
+const status = document.getElementById("status");
+const resultado = document.getElementById("resultado");
+
+btnBuscar.addEventListener("click", buscarClima);
+
